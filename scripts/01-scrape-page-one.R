ad6d9ba4b378b53970574484ffac4eb7af32af4f
sliplr19 <sliplr19@wfu.edu>
2021-03-18 12:03:52 -0400

    Finished page 2

--- scripts/01-scrape-page-one.R
+++ scripts/01-scrape-page-one.R
@@ -32,7 +32,8 @@ links <- page %>%
 artists <- page %>%
   html_nodes(".iteminfo") %>%
   html_node(".artist") %>%
-  html_text()
+  html_text() %>%
+  str_squish()
 
 # put together in a data frame -------------------------------------------------
 
@@ -44,11 +45,40 @@ first_ten <- tibble(
 
 # scrape second ten paintings --------------------------------------------------
 
-second_url <- "___"
+second_url <- "https://collections.ed.ac.uk/art/search/*:*/Collection:%22edinburgh+college+of+art%7C%7C%7CEdinburgh+College+of+Art%22?offset=10"
 
-page <- read_html(second_url)
-...
+# read second page --------------------------------------------------------------
+
+page_2 <- read_html(second_url)
+
+# scrape titles ----------------------------------------------------------------
+
+titles_2 <- page_2 %>%
+  html_nodes(".iteminfo") %>%
+  html_node("h3 a") %>%
+  html_text() %>%
+  str_squish()
+
+# scrape links -----------------------------------------------------------------
+
+links_2 <- page_2 %>%
+  html_nodes(".iteminfo") %>%
+  html_node("h3 a") %>%
+  html_attr("href") %>%
+  str_replace("\\.", "https://collections.ed.ac.uk/art")
+
+# scrape artists ---------------------------------------------------------------
+
+artists_2 <- page_2 %>%
+  html_nodes(".iteminfo") %>%
+  html_node(".artist") %>%
+  html_text() %>%
+  str_squish()
+
+# put together in a data frame -------------------------------------------------
 
 second_ten <- tibble(
-  ...
+  title = titles_2,
+  artist = artists_2,
+  link = links_2
 )
