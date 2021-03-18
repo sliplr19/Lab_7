adb6e4041c6844baf847c069a4e936d21bb2da4e
sliplr19 <sliplr19@wfu.edu>
2021-03-18 12:58:07 -0400

    Finished scraping all.

--- scripts/03-scrape-page-many.R
+++ scripts/03-scrape-page-many.R
@@ -7,12 +7,12 @@ library(glue)
 # list of urls to be scraped ---------------------------------------------------
 
 root <- "https://collections.ed.ac.uk/art/search/*:*/Collection:%22edinburgh+college+of+art%7C%7C%7CEdinburgh+College+of+Art%22?offset="
-numbers <- seq(from = ___, to = ___, by = ___)
-urls <- glue("{___}{___}")
+numbers <- seq(from = 0, to = 2900, by = 10)
+urls <- glue("{root}{numbers}")
 
 # map over all urls and output a data frame ------------------------------------
 
-___ <- map_dfr(___, ___)
+uoe_art <- map_dfr(urls, scrape_page)
 
 # write out data frame ---------------------------------------------------------
 
